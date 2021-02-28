fb5940719697e2a046cc000680380df9e3ed3c4a
Chronex <allancheng0107@gmail.com>
2021-02-28 13:36:18 -0500

    added welcome message to user joining server and when user types start

--- Slack Bot.py
+++ Slack Bot.py
@@ -17,10 +17,72 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SigningSecret'], '/slack/events', app)
-client = slack.WebClient(token=os.environ['slackToken'])
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 bot_id = client.api_call("auth.test")["user_id"]
 
+welcome_messages = {}
+class WelcomeMessage:
+    START_TEXT = {
+        'type': 'section',
+        'text':{
+            'type':'mrkdwn',
+            'text':(
+                'Welcome to this cool Channel! \n\n'
+                '*Get started by completing the tasks!*'
+            )
+
+        }
+    }
+
+    DIVIDER = { 'type': 'divider'}
+
+    def __init__(self, channel, user):
+        self.channel = channel
+        self.user = user
+        self.icon_emoji = ':robot_face:'
+        self.timestamp = ''
+        self.completed = False
+
+    def get_message(self):
+        return {
+            'ts' : self.timestamp,
+            'channel': self.channel,
+            'username': 'Welcome Robot!',
+            'icon_emoji': self.icon_emoji,
+            'blocks': [
+                self.START_TEXT,
+                self.DIVIDER,
+                self._get_reaction_task()
+            ]
+        }
+    
+    def _get_reaction_task(self):
+        checkmark = ':white_check_mark:'
+        if not self.completed:
+            checkmark = ':white_large_square:'
+        
+        text = f'{checkmark}* React to this message!*'
+        return {'type': 'section','text':{'type':'mrkdwn','text': text}}
+
+
+def  send_welcome_message(channel,user):
+    welcome =  WelcomeMessage(channel,user)
+    message = welcome.get_message()
+    response = client.chat_postMessage(**message)
+    welcome.timestamp = response['ts']
+
+    if channel not in welcome_messages:
+        welcome_messages[channel] = {}
+    welcome_messages[channel][user] = welcome
+
+# on member joining server
+@slack_event_adapter.on('team_join')
+def team_join(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    send_welcome_message(channel_id,user_id)
 
 # on message
 @slack_event_adapter.on('message')
@@ -34,8 +96,12 @@ def message(payload):
     if text_block[0] == 'weather':  # if looking for weather
         weather_message = weather_call(text_block[1:])
         client.chat_postMessage(channel=channel_id, text=weather_message)
+
+    elif text_block[0] == 'start':
+        send_welcome_message(channel_id,user_id)
+  
     else:
-        if bot_id != user_id:
+         if user_id != None and bot_id != user_id:
             client.chat_postMessage(channel=channel_id, text=text)
 
 
