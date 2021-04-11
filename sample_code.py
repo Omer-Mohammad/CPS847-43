95de5f2b92c936e65c7b28324c859ec71dff8a78
ho-andy <hoandy1998@gmail.com>
2021-04-11 14:59:50 -0400

    Update sample_code.py

--- sample_code.py
+++ sample_code.py
@@ -1,13 +1,3 @@
-
-import unittest
-
 def multiply_numbers_add_five(first, second):
     return (int(first) * int(second)) + 5
 
-class TestMultiply(unittest.TestCase):
-    def test_multiply_numbers_add_five(self):
-        self.assertEqual(multiply_numbers_add_five(2, 3), 11)
-        self.assertEqual(multiply_numbers_add_five(5, 7), 40)
-        
-if __name__ == '__main__':
-    unittest.main()
